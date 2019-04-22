f89f6a1190af72ad098c915da4b0fd07ca241257
andrewzhj <andrew_zhj@126.com>
2019-04-22 22:41:35 +0800

    add decision tree

---
+++ classifier_tutorial/decision_tree.py
@@ -0,0 +1,39 @@
+# -*- coding: UTF-8 -*-
+'''
+@author: Andrewzhj
+@contact: andrew_zhj@126.com
+@file: decision_tree.py
+@time: 4/22/19 9:49 PM
+@desc: 决策树
+@note:
+'''
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import classification_report
+
+
+def titanic_train():
+    titanic = pd.read_csv('http://biostat.mc.vanderbilt.edu/wiki/pub/Main/DataSets/titanic.txt')
+    print(titanic.head(10))
+    print(titanic.info())
+    x = titanic[['pclass', 'age', 'sex']]
+    y = titanic['survived']
+    x['age'].fillna(x['age'].mean(), inplace=True)
+    x.info()
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=33)
+    vec = DictVectorizer(sparse=False)
+    x_train = vec.fit_transform(x_train.to_dict(orient='record'))
+    print(vec.feature_names_)
+    x_test = vec.transform(x_test.to_dict(orient='record'))
+    dtc = DecisionTreeClassifier()
+    dtc.fit(x_train, y_train)
+    y_predict = dtc.predict(x_test)
+    print(dtc.score(x_test, y_test))
+    print(classification_report(y_predict, y_test, target_names=['died', 'survived']))
+
+
+if __name__ == '__main__':
+    titanic_train()
